bc8e2b31300ff20ee4398d356dd71171ca24dc57
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2024-12-29 22:33:37 +0000

    feat: scaffold initial cli loop

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 import { parseArgs } from "jsr:@std/cli/parse-args";
 import { red } from "jsr:@std/fmt/colors";
+import { select } from "npm:@inquirer/prompts";
+
+type JSONObject = Record<string, string | Record<string, string>>;
+type Choice = { name: string; value: string };
 
 if (import.meta.main) {
   const flags = parseArgs(Deno.args, {
@@ -7,15 +11,62 @@ if (import.meta.main) {
   });
 
   if (!flags.path) {
-    console.log(red("Please provide a path"));
+    console.log(red("Please provide a path to a JSON file"));
     Deno.exit(1);
   }
-
-  console.log(flags.path);
 
   const module = await import(flags.path, {
     with: { type: "json" },
   });
 
-  console.log(module);
+  let currentObj = module.default;
+  let result = "";
+
+  while (true) {
+    const keys = getKeys(currentObj);
+
+    const shouldContinueEarly = keys.length === 1;
+    if (shouldContinueEarly) {
+      const [{ value: newKey }] = keys
+      result = appendToResult(result, newKey);
+
+      if (typeof currentObj[newKey] === "string") {
+        console.log(`Result: ${result}\nnewKey: ${currentObj[newKey]}`);
+        break;
+      }
+
+      currentObj = currentObj[newKey];
+      continue;
+    }
+
+    const newKey = await select({
+      message: "Select a key:",
+      choices: keys,
+    });
+
+    result = appendToResult(result, newKey);
+
+    const shouldBreak = typeof currentObj[newKey] === "string";
+    if (shouldBreak) {
+      console.log(`Result: ${result}\nValue: ${currentObj[newKey]}`);
+      break;
+    }
+
+    currentObj = currentObj[newKey];
+  }
+
+  Deno.exit(0);
+}
+
+function appendToResult(result: string, key: string): string {
+  return result ? `${result}.${key}` : key;
+}
+
+function getKeys(obj: JSONObject): Choice[] {
+  return Object.keys(obj).map((key) => {
+    return {
+      name: key,
+      value: key,
+    };
+  });
 }
